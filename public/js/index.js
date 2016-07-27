68d5c4242059794b90b5eda30d50924e807599fd
Themis Theologitis <theothem@csd.uoc.gr>
2016-07-27 18:34:38 +0300

    v.2

---
+++ public/js/index.js
@@ -0,0 +1,356 @@
+
+            var table = [
+                "iWorx", "iworx.gr", "1.00794", 1, 1,"images/iworx.png",
+                "iWorx", "iworx.gr", "4.002602", 18, 1,"images/iworx.png",
+                "iWorx", "iworx.gr", "6.941", 1, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "9.012182", 2, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "10.811", 13, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "12.0107", 14, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "14.0067", 15, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "15.9994", 16, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "18.9984032", 17, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "20.1797", 18, 2,"images/iworx.png",
+                "iWorx", "iworx.gr", "22.98976...", 1, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "24.305", 2, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "26.9815386", 13, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "28.0855", 14, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "30.973762", 15, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "32.065", 16, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "35.453", 17, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "39.948", 18, 3,"images/iworx.png",
+                "iWorx", "iworx.gr", "39.948", 1, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "40.078", 2, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "44.955912", 3, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "47.867", 4, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "50.9415", 5, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "51.9961", 6, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "54.938045", 7, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "55.845", 8, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "58.933195", 9, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "58.6934", 10, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "63.546", 11, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "65.38", 12, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "69.723", 13, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "72.63", 14, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "74.9216", 15, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "78.96", 16, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "79.904", 17, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "83.798", 18, 4,"images/iworx.png",
+                "iWorx", "iworx.gr", "85.4678", 1, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "87.62", 2, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "88.90585", 3, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "91.224", 4, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "92.90628", 5, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "95.96", 6, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "(98)", 7, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "101.07", 8, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "102.9055", 9, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "106.42", 10, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "107.8682", 11, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "112.411", 12, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "114.818", 13, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "118.71", 14, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "121.76", 15, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "127.6", 16, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "126.90447", 17, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "131.293", 18, 5,"images/iworx.png",
+                "iWorx", "iworx.gr", "132.9054", 1, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "132.9054", 2, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "138.90547", 4, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "140.116", 5, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "140.90765", 6, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "144.242", 7, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "(145)", 8, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "150.36", 9, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "151.964", 10, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "157.25", 11, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "158.92535", 12, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "162.5", 13, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "164.93032", 14, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "167.259", 15, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "168.93421", 16, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "173.054", 17, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "174.9668", 18, 9,"images/iworx.png",
+                "iWorx", "iworx.gr", "178.49", 4, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "180.94788", 5, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "183.84", 6, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "186.207", 7, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "190.23", 8, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "192.217", 9, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "195.084", 10, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "196.966569", 11, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "200.59", 12, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "204.3833", 13, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "207.2", 14, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "208.9804", 15, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "(209)", 16, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "(210)", 17, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "(222)", 18, 6,"images/iworx.png",
+                "iWorx", "iworx.gr", "(223)", 1, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(226)", 2, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(227)", 4, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "232.03806", 5, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "231.0588", 6, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "238.02891", 7, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(237)", 8, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(244)", 9, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(243)", 10, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(247)", 11, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(247)", 12, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(251)", 13, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(252)", 14, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(257)", 15, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(258)", 16, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(259)", 17, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(262)", 18, 10,"images/iworx.png",
+                "iWorx", "iworx.gr", "(267)", 4, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(268)", 5, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(271)", 6, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(272)", 7, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(270)", 8, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(276)", 9, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(281)", 10, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(280)", 11, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(285)", 12, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(284)", 13, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(289)", 14, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(288)", 15, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(293)", 16, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(294)", 17, 7,"images/iworx.png",
+                "iWorx", "iworx.gr", "(294)", 18, 7,"images/iworx.png",
+            ];
+
+            var camera, scene, renderer;
+            var controls;
+
+            var objects = [];
+            var targets = { table: [], sphere: [], helix: [], grid: [] };
+
+            init();
+            animate();
+
+            function init() {
+
+                camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+                camera.position.z = 3000;
+
+                scene = new THREE.Scene();
+
+                // table
+
+                for ( var i = 0; i < table.length; i += 6 ) {
+
+                    var element = document.createElement( 'a' );
+                    element.className = 'element';
+                    //element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
+                    element.href = "http://www."+table[ i + 1];
+                    element.target = "_blank";
+                    element.style.backgroundImage  = "url('images/iworx3.jpg')";
+                    element.style.backgroundRepeat = "no-repeat";
+                    element.style.backgroundSize   = "cover";
+
+                    /*var number = document.createElement( 'div' );
+                    number.className = 'number';
+                    number.textContent = (i/5) + 1;
+                    element.appendChild( number );*/
+
+                    var symbol = document.createElement( 'div' );
+                    symbol.className = 'symbol';
+                    symbol.textContent = table[ i ];
+                    element.appendChild( symbol );
+
+                    //image
+                    /*var image = document.createElement( 'img' );
+                    image.className = 'image';
+                    image.src = table[ i + 5 ];
+                    element.appendChild( image );*/
+
+                    // var details = document.createElement( 'div' );
+                    // details.className = 'details';
+                    // details.innerHTML = table[ i + 1 ] /*+ '<br>' + table[ i + 2 ]*/;
+                    // element.appendChild( details );
+
+                    var object = new THREE.CSS3DObject( element );
+                    object.position.x = Math.random() * 4000 - 2000;
+                    object.position.y = Math.random() * 4000 - 2000;
+                    object.position.z = Math.random() * 4000 - 2000;
+                    scene.add( object );
+
+                    objects.push( object );
+
+                    //
+
+                    var object = new THREE.Object3D();
+                    object.position.x = ( table[ i + 3 ] * 140 ) - 1260;
+                    object.position.y = - ( table[ i + 4 ] * 180 ) + 990;
+
+                    targets.table.push( object );
+
+                }
+
+                // sphere
+
+                var vector = new THREE.Vector3();
+
+                for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+                    var phi = Math.acos( -1 + ( 2 * i ) / l );
+                    var theta = Math.sqrt( l * Math.PI ) * phi;
+
+                    var object = new THREE.Object3D();
+
+                    object.position.x = 800 * Math.cos( theta ) * Math.sin( phi );
+                    object.position.y = 800 * Math.sin( theta ) * Math.sin( phi );
+                    object.position.z = 800 * Math.cos( phi );
+
+                    vector.copy( object.position ).multiplyScalar( 2 );
+
+                    object.lookAt( vector );
+
+                    targets.sphere.push( object );
+
+                }
+
+                // helix
+
+                var vector = new THREE.Vector3();
+
+                for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+                    var phi = i * 0.175 + Math.PI;
+
+                    var object = new THREE.Object3D();
+
+                    object.position.x = 900 * Math.sin( phi );
+                    object.position.y = - ( i * 8 ) + 450;
+                    object.position.z = 900 * Math.cos( phi );
+
+                    vector.x = object.position.x * 2;
+                    vector.y = object.position.y;
+                    vector.z = object.position.z * 2;
+
+                    object.lookAt( vector );
+
+                    targets.helix.push( object );
+
+                }
+
+                // grid
+
+                for ( var i = 0; i < objects.length; i ++ ) {
+
+                    var object = new THREE.Object3D();
+
+                    object.position.x = ( ( i % 5 ) * 400 ) - 800;
+                    object.position.y = ( - ( Math.floor( i / 5 ) % 5 ) * 400 ) + 800;
+                    object.position.z = ( Math.floor( i / 25 ) ) * 1000 - 2000;
+
+                    targets.grid.push( object );
+
+                }
+
+                //
+
+                renderer = new THREE.CSS3DRenderer();
+                renderer.setSize( window.innerWidth, window.innerHeight );
+                renderer.domElement.style.position = 'absolute';
+                document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+                //
+
+                controls = new THREE.TrackballControls( camera, renderer.domElement );
+                controls.rotateSpeed = 0.5;
+                controls.minDistance = 500;
+                controls.maxDistance = 6000;
+                controls.addEventListener( 'change', render );
+
+                var button = document.getElementById( 'table' );
+                button.addEventListener( 'click', function ( event ) {
+
+                    transform( targets.table, 2000 );
+
+                }, false );
+
+                var button = document.getElementById( 'sphere' );
+                button.addEventListener( 'click', function ( event ) {
+
+                    transform( targets.sphere, 2000 );
+
+                }, false );
+
+                var button = document.getElementById( 'helix' );
+                button.addEventListener( 'click', function ( event ) {
+
+                    transform( targets.helix, 2000 );
+
+                }, false );
+
+                var button = document.getElementById( 'grid' );
+                button.addEventListener( 'click', function ( event ) {
+
+                    transform( targets.grid, 2000 );
+
+                }, false );
+
+                transform( targets.table, 2000 );
+
+                //
+
+                window.addEventListener( 'resize', onWindowResize, false );
+
+            }
+
+            function transform( targets, duration ) {
+
+                TWEEN.removeAll();
+
+                for ( var i = 0; i < objects.length; i ++ ) {
+
+                    var object = objects[ i ];
+                    var target = targets[ i ];
+
+                    new TWEEN.Tween( object.position )
+                        .to( { x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration )
+                        .easing( TWEEN.Easing.Exponential.InOut )
+                        .start();
+
+                    new TWEEN.Tween( object.rotation )
+                        .to( { x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration )
+                        .easing( TWEEN.Easing.Exponential.InOut )
+                        .start();
+
+                }
+
+                new TWEEN.Tween( this )
+                    .to( {}, duration * 2 )
+                    .onUpdate( render )
+                    .start();
+
+            }
+
+            function onWindowResize() {
+
+                camera.aspect = window.innerWidth / window.innerHeight;
+                camera.updateProjectionMatrix();
+
+                renderer.setSize( window.innerWidth, window.innerHeight );
+
+                render();
+
+            }
+
+            function animate() {
+
+                requestAnimationFrame( animate );
+
+                TWEEN.update();
+
+                controls.update();
+
+            }
+
+            function render() {
+
+                renderer.render( scene, camera );
+
+            }
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>three.js css3d - periodic table. info.</title>
+    
+    
+    
+        <link rel="stylesheet" href="css/index.css">
+    
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    <div id="container"></div>
+<div id="menu">
+            <button id="table">TABLE</button>
+            <button id="sphere">SPHERE</button>
+            <button id="helix">HELIX</button>
+            <button id="grid">GRID</button>
+        </div>
+    <script src='http://threejs.org/build/three.min.js'></script>
+<script src='http://threejs.org/examples/js/libs/tween.min.js'></script>
+<script src='http://threejs.org/examples/js/controls/TrackballControls.js'></script>
+<script src='http://threejs.org/examples/js/renderers/CSS3DRenderer.js'></script>
+
+        <script src="js/index.js"></script>
+
+    
+    
+    
+  </body>
+</html>
